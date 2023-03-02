fc1b9f3f463971c60072e5f1655b5e17e8a27866
Anjali Kumar <anjalik02@tamu.edu>
2023-03-02 15:07:38 -0600

    finished dropdown

--- sketch.js
+++ sketch.js
@@ -1,18 +1,47 @@
-let button, img;
+let button, img, sel;
 
 function preload() {
-  img = loadImage('assets/castle.png');
+  img = loadImage('assets/Robot.png');
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1300, 800);
+
+  // box 1
   button = createButton('click me');
   button.position(19, 19);
   button.mousePressed(loadCamera);
+
+  //box 2
+  fill('white');
+  textSize(32);
+  text('What is your \nfavorite AI assistant?', 450, 49);
+  sel = createSelect();
+  sel.position(450, 150);
+  sel.option('Select')
+  sel.option('Alexa');
+  sel.option('Siri');
+  sel.option('Cortana');
+  sel.changed(mySelectEvent);
+}
+
+function mySelectEvent() {
+  background('#1b1b1b'); 
+
+  fill('white');
+  textSize(32)
+  text('What is your \nfavorite AI assistant?', 450, 49);
+  
+  if(sel.value() == "Select"){
+    text("Please select", 450, 300)
+  }
+  else{
+    text("Your favorite is \n" + sel.value(), 450, 300)
+  }
 }
 
 function loadCamera(){
-  image(img, 0, 0);
+  image(img, 70, 20);
 }
 
 
